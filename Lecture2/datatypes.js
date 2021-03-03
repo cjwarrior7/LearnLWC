3920cb68beb185ca16883668db85844b3cb4e949
cjwarrior7 <cj23021996@gmail.com>
2021-03-03 23:25:28 +0530

    JS Spread operator and other topics

---
+++ Lecture2/datatypes.js
@@ -0,0 +1,38 @@
+var v=10.99;
+console.log(typeof v);
+
+var y=10n;
+console.log(typeof y);
+
+var name='cj';
+console.log(typeof name);
+
+var isMonday=true;
+console.log(typeof isMonday);
+
+var z;
+console.log(typeof z);
+
+var obj={ };
+console.log(typeof obj);
+
+var arr =[];
+console.log(typeof arr);
+
+var sym=Symbol("id");
+console.log(typeof sym);
+// null and remember type of null is object
+var u = 10;
+u = null;
+console.log(typeof u);
+
+//Null vs Undefined
+var q;
+console.log(q);
+var r = null;
+console.log(r);
+console.log(q == r);
+console.log(q === r);
+
+
+
---
+++ Lecture3/script.js
@@ -0,0 +1,51 @@
+// array
+
+var arr = [1,2,3,4,5];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+//objects
+var obje = {
+    "name" : "cj",
+    "full name" :"cj warrior",
+    "age" : 23
+}
+console.log(obje.name);
+console.log(obje.age);
+console.log(obje["full name"]); // this is use case when we have space in key we access it like array
+obje.hobbies = "reading"
+console.log(obje)
+
+//Spread operator
+//1.Expanding the operator
+let greeting = "Hicjwarrior7";
+let charlist = [...greeting];
+console.log(charlist);
+
+//2.Combining  Array
+let var89 = ["amazon","flipkart"];
+let var87 = ["facebook","Instagram"];
+let var67 = [...var89 ,...var87];
+console.log(var67);
+
+//3.Adding values to an array
+let var90 = ["amazon","flipkart","facebook"];
+let var88 = [...var90,"Walmart"];
+console.log(var88);
+ var88 = ["Paytm",...var90];
+console.log(var88);
+
+//4.Combining Objects
+let obj1={name:"salesforce",age:23,date:"23/02/2021"};
+let obj2={name:"copado",age:25,data:"test"};
+let obj3={...obj1,...obj2};
+console.log(obj3);
+
+//5.Shallow copy
+
+
+
+
+
